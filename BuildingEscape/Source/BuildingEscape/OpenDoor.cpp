6a478ef441f7a34f4748a4d5fdb3e2cc3a896956
Mehmet Cengiz <m.cengiz4716@gmail.com>
2017-02-04 02:40:37 +0300

    Table, Chair have mass.Doors are open when they on plate.

--- BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
+++ BuildingEscape/Source/BuildingEscape/OpenDoor.cpp
@@ -43,7 +43,7 @@ void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorCompon
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	// Poll the Trigger Volume
-	if (GetTotalMassOfActorsOnPlate() > 50.f){
+	if (GetTotalMassOfActorsOnPlate() > 30.f){
 		OpenDoor();
 		LastDoorOpenTime = GetWorld()->GetTimeSeconds();
 	}
@@ -57,6 +57,12 @@ float UOpenDoor::GetTotalMassOfActorsOnPlate(){
 	TArray<AActor*> OverlappingActors;
 	PressurePlate->GetOverlappingActors(OUT OverlappingActors);
 	
+	//Iterate through them adding their masses.
+	for(const auto& Actor : OverlappingActors) {
+		TotalMass += Actor->FindComponentByClass<UPrimitiveComponent>()->GetMass();
+		UE_LOG(LogTemp,Warning,TEXT("%s on pressure plate"), *Actor->GetName())
+	}
+
 	return TotalMass;
 }
 
